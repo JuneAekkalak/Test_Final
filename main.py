4f305adf13bbc88f8669624aae1619442dd2b385
JuneAekkalak <aekkalak.ju@gmail.com>
2023-03-27 11:33:48 +0700

    edit main

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-from function import process_weight,sum_of_price
-input_orange = float(input("orange: "))
-input_mango = float(input("mango: "))
-input_watermelon = float(input("watermelon: "))
+from function import sum_of_price
+input_orange = input("orange: ")
+input_mango = input("mango: ")
+input_watermelon = input("watermelon: ")
+
+try:
+    input_orange = float(input_orange)
+    input_mango = float(input_mango)
+    input_watermelon = float(input_watermelon)
+except:
+    input_orange = str(input_orange)
+    input_mango = str(input_mango)
+    input_watermelon = str(input_watermelon)
+
+
 result = sum_of_price(input_orange,input_mango,input_watermelon)
 print(result)
