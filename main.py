428d75a2718f0a4d6fa8e8685ada86db1796587c
JuneAekkalak <aekkalak.ju@gmail.com>
2023-03-27 11:54:57 +0700

    edit main

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 from function import sum_of_price
+
 input_orange = input("orange: ")
 input_mango = input("mango: ")
 input_watermelon = input("watermelon: ")
 
 try:
     input_orange = float(input_orange)
-    input_mango = float(input_mango)
-    input_watermelon = float(input_watermelon)
-except:
+except ValueError:
     input_orange = str(input_orange)
+
+try:
+    input_mango = float(input_mango)
+except ValueError:
     input_mango = str(input_mango)
+
+try:
+    input_watermelon = float(input_watermelon)
+except ValueError:
     input_watermelon = str(input_watermelon)
 
-
-result = sum_of_price(input_orange,input_mango,input_watermelon)
+result = sum_of_price(input_orange, input_mango, input_watermelon)
 print(result)
